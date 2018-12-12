d595402fcae402656756f5ff6b3a6189a171c510
amanshawar <32957970+amanshawar@users.noreply.github.com>
2018-12-12 11:24:48 +0200

    Delete server.js
    delete server.js

--- src/server/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const router = require('./router.js');
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer(router)
-server.listen(PORT,()=>{
-  console.log(` The server up and running on port ${PORT}!`);
-});
